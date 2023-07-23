9be2498e5f3a117251fb4dac4e9a298d89feec12
Amjad Hossain Piash <140346920+hossainpiash2221@users.noreply.github.com>
2023-07-24 05:30:43 +0600

    Add files via upload

---
+++ ternaryOperator.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int a=10;
+    int b=2;
+
+    if(int c=a<b ? a:b)
+    {
+
+        cout<<c<<endl;
+    }
+
+    int x =(1,2,100);
+    cout<<x<<endl;
+}
