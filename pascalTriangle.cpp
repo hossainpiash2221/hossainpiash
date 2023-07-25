9d6a9a285cde50232a71feb8708062f691eaa802
Amjad Hossain Piash <140346920+hossainpiash2221@users.noreply.github.com>
2023-07-26 03:08:39 +0600

    Add files via upload
    1
    1 1
    1 2 1
    1 3 3 1

---
+++ pascalTriangle.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+
+int fact(int num)
+{
+    int temp=1;
+    for(int i=2;i<=num;i++)
+    {
+        temp*=i;
+    }
+    return temp;
+}
+int main()
+{
+    int n;
+    cin>>n;
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j =0;j<=i;j++)
+        {
+            cout<<(fact(i)/(fact(i-j)*fact(j)))<<" ";
+        }
+        cout<<endl;
+    }
+}
